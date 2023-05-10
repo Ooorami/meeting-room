0a5e26791d84d952b21510ae47fae14dfdf8ed28
syCheon <cheonshin99@hs.ac.kr>
2023-05-10 13:29:17 +0900

    [fix] .idea 폴더 제거(캐시파일 다른 컴퓨터에서 다르게 동작할 가능성이 있음)

--- server/src/main/kotlin/org/meeting/reservation/Application.kt
+++
@@ -1,5 +0,0 @@
-@SpringBootApplication
-open class Application
-fun main(args: Array<String>) {
-    SpringApplication.run(Application::class.java, *args)
-}
--- server/src/main/kotlin/org/meeting/reservation/controller/RoomController.kt
+++
@@ -1,5 +0,0 @@
-
-@RestController
-class RoomController() {
-
-}
